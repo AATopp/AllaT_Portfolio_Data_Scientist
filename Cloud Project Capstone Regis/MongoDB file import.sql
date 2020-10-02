ad4f619acd5968d1898c5bf1a769ce264c114f50
Alla Topp <59368945+AATopp@users.noreply.github.com>
2020-10-02 13:57:19 -0400

    Create MongoDB file import.sql

---
+++ Cloud Project Capstone Regis/MongoDB file import.sql
@@ -0,0 +1,5 @@
+mongoimport -d yelp -c checkin checkin.json
+mongoimport -d yelp -c business business.json
+mongoimport -d yelp -c tip tip.json
+mongoimport -d yelp -c user user.json
+mongoimport -d yelp -c review review.json
