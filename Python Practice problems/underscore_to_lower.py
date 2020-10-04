6d5647200e8379c4ea197df016866b496cbdd66c
Alla Topp <59368945+AATopp@users.noreply.github.com>
2020-10-03 22:00:51 -0400

    Create underscore_to_lower.py

---
+++ Python Practice problems/underscore_to_lower.py
@@ -0,0 +1,18 @@
+# Convert text from the frase with underscores to lower letter
+# A. Topp
+
+s = "hello_string_processing_InPython"
+
+new_string = ""
+
+capitalize_next = True
+for character in s:
+    if capitalize_next:
+        new_string += character.upper()
+        capitalize_next = False
+    elif character == '_':
+        capitalize_next = True
+    else:
+        new_string += character
+
+print(new_string)
