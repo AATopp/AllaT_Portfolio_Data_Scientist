3e17903ade79e59fa505b383b962d49961f534fd
Alla Topp <59368945+AATopp@users.noreply.github.com>
2020-10-13 21:59:34 -0400

    Create what day is it.py

---
+++ Python Practice problems/what day is it.py
@@ -0,0 +1,8 @@
+
+# Read a date from the input given in the following format: YYYY-MM-DD. 
+# Print the year, month and day on separate lines.date = '2020-04-30'
+
+dates = date.split('-')
+
+for i in dates:
+    print(i)
